65ffb33ce4f1eb129c51a371953cc05366f75466
unknown <foysalk877@gmail.com>
2023-10-04 15:01:25 +0600

    done for server

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// const express = require("express");
-// const cors = require("cors");
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// const dotenv = require("dotenv").config();
-// const app = express();
-// const path=require('path')
-// const PORT=process.env.PORT||4000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dotenv = require("dotenv").config();
+const app = express();
+const path=require('path')
+const PORT=process.env.PORT||4000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
